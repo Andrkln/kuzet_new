883c6a592c0efe727e13c828faf1dac73a6a0b69
Andrkln <andrew13338@gmail.com>
2025-03-07 23:47:05 +0500

    fgfgfgf

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ const aleo = Aleo({
 export const metadata = {
   metadataBase: new URL("https://kuzet-standart.kz/"),
   title: "Кузет Стандарт - Стандарт безопасности",
-  description: "Профессиональные услуги охраны для домов, бизнеса и объектов. Быстрая реакция, круглосуточный мониторинг и лучшие сотрудники.",
+  description: "Профессиональные услуги охраны для домов, бизнеса и объектов. Быстрая реакция, круглосуточный мониторинг и лучшие сотрудники",
   openGraph: {
     title: "Кузет Стандарт - Охрана Алматы",
     description: "Мы обеспечиваем защиту вашего дома, бизнеса и имущества с самыми быстрыми группами реагирования в городе",
@@ -36,12 +36,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={aleo.className}>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body suppressHydrationWarning>
         <ChakraProvider>
-        <ChatPlace />
+          <ChatPlace />
           {children}
-          </ChakraProvider>
+        </ChakraProvider>
       </body>
     </html>
   );
 }
+
